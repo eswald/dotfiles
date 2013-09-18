acd2b7eb3b31f61ea043740841d3f93dbfbb0cca
Eric Wald <eswald@gmail.com>
2013-09-17 21:47:49 -0600

    More reliable ssh agent setting.

--- .config/bash/agent.sh
+++ .config/bash/agent.sh
@@ -1,5 +1,30 @@
 # Collect SSH agent credentials if available.
+# Based on http://hintsforums.macworld.com/showthread.php?t=50836
 if [ -z "$SSH_AGENT_PID" -o -z "$SSH_AUTH_SOCK" ]
 then
-  eval $(ssh-agent) > /dev/null
+  SSH_AGENT_CACHE="$HOME/.cache/ssh-agent"
+  if [ -s "${SSH_AGENT_CACHE}" ]
+  then
+    # Try to re-use an existing agent.
+    source "${SSH_AGENT_CACHE}" > /dev/null
+    
+    # Check that the agent is still running.
+    # Consider checking the pipe, too.
+    if ! kill -0 "${SSH_AGENT_PID}" 2> /dev/null
+    then
+      # Looks like the SSH-Agent has died; it'll be restarted below.
+      rm -f "${SSH_AGENT_CACHE}"
+    fi
+  fi
+  
+  if [ ! -f "${SSH_AGENT_CACHE}" ]
+  then
+    # Start a new agent.
+    mkdir -p "$HOME/.cache"
+    touch "${SSH_AGENT_CACHE}"
+    chmod 600 "${SSH_AGENT_CACHE}"
+    ssh-agent > "${SSH_AGENT_CACHE}"
+    chmod 400 "${SSH_AGENT_CACHE}"
+    source "${SSH_AGENT_CACHE}" > /dev/null
+  fi
 fi
