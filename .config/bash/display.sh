41a945b6c36b0189fc18f7e5bcc0603d3fef34ae
Eric Wald <eswald@gmail.com>
2014-04-12 20:41:49 -0600

    Repairing the X display check.
    USERNAME isn't always set; USER is probably more common.

--- .config/bash/display.sh
+++ .config/bash/display.sh
@@ -1,11 +1,11 @@
 # Let graphical programs know where to show up.
 # This used to be based on $GDM_XSERVER_LOCATION,
 # but that doesn't seem to be available on Koala.
-if [ -z "$DISPLAY" ] && [ "$USERNAME" = "eswald" ]
+if [ -z "$DISPLAY" ] && [ "$USER" = "eswald" ]
 then
   xline="\/\(bin\|X11\)\/X :"
   display=$(ps -AH -ouser,cmd | \
-    sed "/$xline/{x;d};/^root /d;/^$USERNAME /{x;/$xline/p;d};x;d" | \
+    sed "/$xline/{x;d};/^root /d;/^$USER /{x;/$xline/p;d};x;d" | \
     head -n 1 | cut -d: -f2 | cut -d' ' -f1)
   if [ -n "$display" ]; then
     export DISPLAY=:$display
